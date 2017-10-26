dcd6f158d3a7ecb29a637a59c999c367fadc7284
luoxi <iceluo@gmail.com>
2017-10-26 12:01:51 +0900

    fix NewDeck ui

--- project-mobile-flashcards/components/NewDeck.js
+++ project-mobile-flashcards/components/NewDeck.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { FormLabel, FormInput } from "react-native-elements";
+
+import { gray, white } from "../utils/colors";
 
 class NewDeck extends Component {
   static navigationOptions = {
@@ -7,8 +10,42 @@ class NewDeck extends Component {
   };
 
   render() {
-    return <Text>New Deck</Text>;
+    return (
+      <View style={{ flex: 1 }}>
+        <FormLabel>What is the title of your new deck?</FormLabel>
+        <FormInput onChangeText={() => console.log("onChangeText")} />
+        <View style={[styles.center, { justifyContent: "flex-start" }]}>
+          <TouchableOpacity
+            style={[styles.btn, { backgroundColor: white }]}
+            onPress={() => console.log("new deck")}
+          >
+            <Text style={[styles.btnText, { color: gray }]}>Submit</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 30,
+    marginRight: 30
+  },
+  btn: {
+    padding: 10,
+    backgroundColor: gray,
+    alignSelf: "center",
+    borderRadius: 5,
+    margin: 5
+  },
+  btnText: {
+    color: white,
+    fontSize: 20
+  }
+});
 
 export default NewDeck;
